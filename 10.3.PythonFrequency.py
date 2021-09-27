d2d554e2f6589942530d45dacfa2c6b62a2bbd37
Nikita B Kiselev <nikita.b.kiselev@mail.ru>
2021-09-27 22:30:26 +0500

    Initial commit

---
+++ 10.3.PythonFrequency.py
@@ -0,0 +1,26 @@
+import requests, datetime, pprint
+from datetime import timedelta
+from datetime import datetime
+from pprint import pprint
+
+
+def get_questions(days, tag):
+    end_date = int(datetime.timestamp(datetime.now()))
+    start_date = end_date - days * 86400
+
+    PARAMS = {
+        'fromdate': start_date,
+        'todate': end_date,
+        'tagged': tag,
+        'site': 'stackoverflow'
+    }
+
+    resp = requests.get('https://api.stackexchange.com/2.2/questions', params=PARAMS)
+    pprint(resp)
+    for question in resp.json().get('items'):
+        print(('Question: ' + question['title']).upper())
+        print('Tags: ' + str(question['tags']))
+        print('-------------------------')
+
+
+get_questions(2, 'python')
